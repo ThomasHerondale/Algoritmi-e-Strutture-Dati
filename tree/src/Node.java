6fe8d254fc3c8798c0197723aadce03c0625871c
ThomasHerondale <thomasherondale@gmail.com>
2021-11-29 23:09:14 +0100

    Getter in Node

--- tree/src/Node.java
+++ tree/src/Node.java
@@ -2,8 +2,20 @@ public class Node<T> {
     private int key;
     private T data;
     // Parent?
-    private Node<T> dx;
     private Node<T> sx;
+    private Node<T> dx;
+
+    public int getKey() {
+        return key;
+    }
+
+    public Node<T> getSx() {
+        return sx;
+    }
+
+    public Node<T> getDx() {
+        return dx;
+    }
 
 
 }
