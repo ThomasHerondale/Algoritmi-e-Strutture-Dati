f02faff6bd15b3e3841ffc7738767647bf3e1e9f
ThomasHerondale <thomasherondale@gmail.com>
2021-11-29 23:00:36 +0100

    Creazione file e impostazione classi

---
+++ tree/src/BinarySearchTree.java
@@ -0,0 +1,19 @@
+public class BinarySearchTree<T> {
+    private Node<T> root;
+
+    public void insert(Node<T> node) {
+
+    }
+
+    public Node<T> delete(int key) {
+
+    }
+
+    public boolean search(int key) {
+
+    }
+
+    public void print() {
+
+    }
+}
---
+++ tree/src/Node.java
@@ -0,0 +1,9 @@
+public class Node<T> {
+    private int key;
+    private T data;
+    // Parent?
+    private Node<T> dx;
+    private Node<T> sx;
+
+
+}
