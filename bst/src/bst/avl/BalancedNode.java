5a34a67e06992036fd7d36f76a02acfb29443c2f
ThomasHerondale <thomasherondale@gmail.com>
2021-12-01 15:19:38 +0100

    Metodi inerenti al fattore di bilanciamento del nodo in BalancedNode

--- bst/src/bst/avl/BalancedNode.java
+++ bst/src/bst/avl/BalancedNode.java
@@ -9,4 +9,28 @@ public class BalancedNode<T> extends Node<T> {
         super(key, data);
         this.balanceFactor = 0;
     }
+
+    public int getBalanceFactor() {
+        return balanceFactor;
+    }
+
+    public void updateBalanceFactor() {
+        this.balanceFactor = getSubtreeHeight(this.getSx()) - getSubtreeHeight(this.getDx());
+    }
+
+    private int getSubtreeHeight(Node<T> current) {
+        return getSubtreeHeight(current, 1);
+    }
+
+    private int getSubtreeHeight(Node<T> current, int currentHeight) {
+        if (current.childrenCount() == 0)
+            return currentHeight;
+        int sxHeight = 0;
+        if (current.getSx() != null)
+            sxHeight = getSubtreeHeight(current.getSx(), currentHeight + 1);
+        int dxHeight = 0;
+        if (current.getDx() != null)
+            dxHeight = getSubtreeHeight(current.getDx(), currentHeight + 1);
+        return Math.max(sxHeight, dxHeight);
+    }
 }
