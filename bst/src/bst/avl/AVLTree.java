2965e605a6c9b80ad64e08ba77f02f62e67606a1
ThomasHerondale <thomasherondale@gmail.com>
2021-12-04 18:28:16 +0100

    insert()

--- bst/src/bst/avl/AVLTree.java
+++ bst/src/bst/avl/AVLTree.java
@@ -3,15 +3,125 @@ package bst.avl;
 import bst.BinarySearchTree;
 import bst.Node;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class AVLTree<T> extends BinarySearchTree<T> {
+    private final Deque<BalancedNode<T>> path;
 
     public AVLTree(int key, T data) {
         super(new BalancedNode<>(key, data));
+        this.path = new ArrayDeque<>();
     }
 
     @Override
-    public void insert(Node<T> newNode) {
-        super.insert(newNode);
+    public BalancedNode<T> getRoot() {
+        return (BalancedNode<T>) super.getRoot();
+    }
+
+    @Override
+    public BalancedNode<T> insert(Node<T> newNode) {
+        BalancedNode<T> parent = (BalancedNode<T>) super.insert(newNode);
+        // Ricalcola i fattori di bilanciamento lungo il cammino percorso per l'inserimento
+        path.push(parent);
+        while (!path.isEmpty()) {
+            BalancedNode<T> currentNode = path.pop();
+            currentNode.updateBalanceFactor();
+            // Se il nodo è sbilanciato, ribilancialo
+            if (Math.abs(currentNode.getBalanceFactor()) >= 2)
+                rebalance(currentNode);
+        }
+        return parent;
+    }
+
+    @Override
+    protected Node<T> findParent(Node<T> childNode) {
+        if (childNode == getRoot())
+            return null;
+        BalancedNode<T> currentNode = getRoot();
+        while (true) {
+            path.push(currentNode);
+            if (childNode.getKey() <= currentNode.getKey()) {
+                if (currentNode.getSx() == null || childNode.equals(currentNode.getSx()))
+                    return currentNode;
+                else
+                    currentNode = (BalancedNode<T>) currentNode.getSx();
+            } else {
+                if (currentNode.getDx() == null || childNode.equals(currentNode.getDx()))
+                    return currentNode;
+                else
+                    currentNode = (BalancedNode<T>) currentNode.getDx();
+            }
+        }
+    }
+
+    private void rebalance(BalancedNode<T> criticalNode) {
+        System.out.println(criticalNode + " è sbilanciato!");
+        // Il nodo è sbilanciato a sinistra?
+        if (criticalNode.getBalanceFactor() > 0) {
+            BalancedNode<T> sx = (BalancedNode<T>) criticalNode.getSx();
+            // TODO balanceFactor
+            if (sx.getSubtreeHeight(sx.getSx()) > sx.getSubtreeHeight(sx.getDx())) {  // SX -- SX
+                System.out.println("Ruoto " + criticalNode + " a destra");
+                rotateRight(criticalNode);
+            } else {    // DX -- SX
+                System.out.println("Composta");
+                System.out.println("Ruoto " + criticalNode.getSx() + " a sinistra");
+                rotateLeft(criticalNode.getSx());
+                System.out.println("Ruoto " + criticalNode + " a destra");
+                rotateRight(criticalNode);
+            }
+        } else {    // Il nodo è sbilanciato a destra
+            BalancedNode<T> dx = (BalancedNode<T>) criticalNode.getDx();
+            if (dx.getSubtreeHeight(dx.getSx()) > dx.getSubtreeHeight(dx.getDx())) {    // SX -- DX
+                System.out.println("Composta");
+                System.out.println("Ruoto " + criticalNode.getDx() + " a destra");
+                rotateRight(criticalNode.getDx());
+                System.out.println("Ruoto " + criticalNode + " a sinistra");
+                rotateLeft(criticalNode);
+            } else {    // DX -- DX
+                System.out.println("Ruoto " + criticalNode + " a sinistra");
+                rotateLeft(criticalNode);
+            }
+        }
+
+        System.out.println("--------");
+        this.print();
+        System.out.println("--------");
+
+        // Controlla che il nodo si sia bilanciato davvero
+        assert Math.abs(criticalNode.getBalanceFactor()) <= 2;
+    }
+
+    private void rotateLeft(Node<T> pivot) {
+        var parent = findParent(pivot);
+        var child = pivot.getDx();
+        if (pivot == getRoot())
+            setRoot(child);
+        // Non dobbiamo occuparci del padre del perno, se il perno è la radice
+        if (parent != null) {
+            if (pivot.getKey() <= parent.getKey())
+                parent.setSx(child);
+            else
+                parent.setDx(child);
+        }
+        pivot.setDx(child.getSx());
+        child.setSx(pivot);
+    }
+
+    private void rotateRight(Node<T> pivot) {
+        var parent = findParent(pivot);
+        var child = pivot.getSx();
+        if (pivot == getRoot())
+            setRoot(child);
+        if (parent != null) {
+            if (pivot.getKey() <= parent.getKey())
+                parent.setSx(child);
+            else
+                parent.setDx(child);
+        }
+        pivot.setSx(child.getDx());
+        child.setDx(pivot);
     }
 
     @Override
--- bst/src/bst/avl/BalancedNode.java
+++ bst/src/bst/avl/BalancedNode.java
@@ -2,6 +2,8 @@ package bst.avl;
 
 import bst.Node;
 
+import java.util.Objects;
+
 public class BalancedNode<T> extends Node<T> {
     private int balanceFactor;
 
@@ -18,8 +20,11 @@ public class BalancedNode<T> extends Node<T> {
         this.balanceFactor = getSubtreeHeight(this.getSx()) - getSubtreeHeight(this.getDx());
     }
 
-    private int getSubtreeHeight(Node<T> current) {
-        return getSubtreeHeight(current, 1);
+    public int getSubtreeHeight(Node<T> current) {
+        if (current != null)
+            return getSubtreeHeight(current, 1);
+        else
+            return 0;
     }
 
     private int getSubtreeHeight(Node<T> current, int currentHeight) {
@@ -32,5 +37,19 @@ public class BalancedNode<T> extends Node<T> {
         if (current.getDx() != null)
             dxHeight = getSubtreeHeight(current.getDx(), currentHeight + 1);
         return Math.max(sxHeight, dxHeight);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        BalancedNode<?> that = (BalancedNode<?>) o;
+        return balanceFactor == that.balanceFactor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), balanceFactor);
     }
 }
